33f6004fab88fc3fd6a50ae3b97a7ea5775e48c1
Rebecca Louw <103456571+rebeccalouw@users.noreply.github.com>
2025-09-25 10:42:17 +0200

    Add files via upload

---
+++ What_Comes_Next/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Challenge: What Comes Next
+Console.WriteLine("Hello, World!");
+Console.WriteLine("I'm trying to learn something new.");
