f83ca2117f3f33bcbd6daac181bfec67d7ff6c92
Rebecca Louw <103456571+rebeccalouw@users.noreply.github.com>
2025-09-25 10:35:13 +0200

    Add files via upload

---
+++ Repairing_The_Clocktower/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Challenge: Repairing the Clocktower
+
+Console.Write("Please enter a number: ");
+int input = Convert.ToInt32(Console.ReadLine());
+
+if (input % 2 == 0) {
+    Console.WriteLine("Tick");
+}
+else {
+    Console.WriteLine("Tock");
+};
---
+++ Taking_A_Number/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Challenge: Taking a Number
+
+
+// Added this method to Buying_Inventory Challenge
+
+int AskForNumber(string text)
+{
+    Console.WriteLine(text);
+    int response = Convert.ToInt32(Console.ReadLine());
+    return response;
+}
+
+
+// Added this method to The_Prototype Challenge 
+
+int AskForNumberInRange(string text, int min, int max)
+{
+    int response;
+    do
+    {
+        Console.Write($"{text} ({min}..{max}): ");
+        response = Convert.ToInt32(Console.ReadLine());
+    }
+    while (response < min || response > max);
+
+    return response;
+}
