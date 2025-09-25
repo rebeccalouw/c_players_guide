25a008eb8bf67b92d258debb5f7d35f2b2e2624b
rebeccalouw <louw.rebecca.m@gmail.com>
2025-09-25 20:50:57 +0200

    Add Countdown console project

---
+++ Countdown/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Challenge: Countdown
+
+void Countdown(int number)
+{
+    if (number <= 0) return ;
+    Console.WriteLine(number);
+    Countdown(number - 1);
+}
+
+Countdown(10);
+
