b2252fa67483d66677c4add41df694bd54d1f4cd
rebeccalouw <louw.rebecca.m@gmail.com>
2025-10-05 16:15:51 +0200

    New console application created

---
+++ The_Point/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Boss Battle: The Point
+
+Point one = new Point(2, 3);
+Point two = new Point(-4, 0);
+
+Console.WriteLine($"Point one: ({one.X}, {one.Y})");
+Console.WriteLine($"Point two: ({two.X}, {two.Y})");
+
+public class Point {
+    public float X { get; }
+    public float Y { get; }
+
+    public Point(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+    public Point()
+    {
+        X = 0;
+        Y = 0;
+    }
+
+}
