115047fb33a1a17bb89e9f30979e31fea88f2250
rebeccalouw <louw.rebecca.m@gmail.com>
2025-10-07 09:12:21 +0200

    New console applications added

---
+++ The_Card/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Boss Battle: The Card
+
+using System.Drawing;
+
+CardColor[] colorsArray = new CardColor[]
+{
+    CardColor.Red,
+    CardColor.Green,
+    CardColor.Blue,
+    CardColor.Yellow,
+};
+
+CardRank[] ranksArray = new CardRank[]
+{
+    CardRank.One,
+    CardRank.Two,
+    CardRank.Three,
+    CardRank.Four,
+    CardRank.Five,
+    CardRank.Six,
+    CardRank.Seven,
+    CardRank.Eight,
+    CardRank.Nine,
+    CardRank.Ten,
+    CardRank.DollarSign,
+    CardRank.Percent,
+    CardRank.Ampersand,
+    CardRank.Caret
+};
+
+foreach (CardColor color in colorsArray)
+{
+    foreach (CardRank rank in ranksArray)
+    {
+        Card card = new Card(rank, color);
+        Console.WriteLine($"The {card.Color} {card.Rank}");
+    }}
+
+public class Card
+{
+    public CardColor Color { get; }
+    public CardRank Rank { get; }
+
+    public Card(CardRank rank, CardColor color)
+    {
+        Rank = rank;
+        Color = color;
+    }
+
+    public bool IsSymbol => Rank == CardRank.Ampersand || Rank == CardRank.Caret || Rank == CardRank.DollarSign ||
+                            Rank == CardRank.Percent;
+
+    public bool IsNumber => !IsSymbol;
+}
+
+public enum CardColor { Red, Green, Blue, Yellow };
+public enum CardRank { One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, DollarSign, Percent, Caret, Ampersand };
---
+++ The_Color/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Boss Battle: The Color
+
+Color turquoise = new Color(64, 224, 208);
+Color purple = Color.CreateColorPurple();
+
+Console.WriteLine($"Turquoise: R={turquoise.R}, G={turquoise.G}, B={turquoise.B}");
+Console.WriteLine($"Purple: R={purple.R}, G={purple.G}, B={purple.B}");
+
+public class Color
+{
+    public byte R { get; }
+    public byte G { get; }
+    public byte B { get; }
+
+    public Color(byte r, byte g, byte b)
+    {
+        R = r;
+        G = g;
+        B = b;
+    }
+    
+    public Color() : this(0, 0, 0) { }
+    
+    public static Color CreateColorWhite() => new Color(255, 255, 255);
+    public static Color CreateColorBlack() => new Color(0, 0, 0);
+    public static Color CreateColorRed() => new Color(255, 0, 0);
+    public static Color CreateColorOrange() => new Color(255, 165, 0);
+    public static Color CreateColorYellow() => new Color(255, 255, 0);
+    public static Color CreateColorGreen() => new Color(0, 128, 0);
+    public static Color CreateColorBlue() => new Color(0, 0, 255);
+    public static Color CreateColorPurple() => new Color(128, 0, 128);
+}
