1a5361e2946bfd0d41c809d8c7f93da04634f4ba
rebeccalouw <louw.rebecca.m@gmail.com>
2025-10-10 12:56:08 +0200

    New challenges added

---
+++ Robotic_Interface/Program.cs
@@ -0,0 +1,94 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Challenge: Robotic Interface
+
+Robot robot = new Robot();
+
+Console.WriteLine("Hello, what should the robot do?");
+Console.WriteLine("Enter a command: on, off, north, south, east, west.");
+
+for (int i = 0; i < robot.Commands.Length; i++)
+{   
+    Console.WriteLine($"Command {i + 1}:");
+    string? choice = Console.ReadLine();
+    IRobotCommand newCommand = choice switch
+    {
+        "on" => new OnCommand(),
+        "off" => new OffCommand(),
+        "north" => new NorthCommand(),
+        "south" => new SouthCommand(),
+        "east" => new EastCommand(),
+        "west" => new WestCommand()
+    };
+    
+    robot.Commands[i] = newCommand;
+}
+
+robot.Run();
+public class OffCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        robot.IsPowered = false;
+        Console.WriteLine("Robot is now turned off.");
+    }
+}
+
+public class OnCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        robot.IsPowered = true;
+        Console.WriteLine("Robot is powered.");
+    }
+}
+
+public class NorthCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.Y++;
+    }
+}
+
+public class SouthCommand : IRobotCommand {
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.Y--;
+    }
+}
+
+public class EastCommand : IRobotCommand {
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.X++;
+    }
+}
+
+public class WestCommand : IRobotCommand {
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.X--;
+    }
+}
+
+public interface IRobotCommand
+{
+    void Run(Robot robot);
+}
+
+public class Robot
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsPowered { get; set; }
+    public IRobotCommand?[] Commands { get; } = new IRobotCommand?[3];
+    public void Run()
+    {
+        foreach (IRobotCommand? command in Commands)
+        {
+            command?.Run(this);
+            Console.WriteLine($"[{X} {Y} {IsPowered}]");
+        }
+    }
+}
---
+++ Room_Coordinates/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Challenge: Room Coordinates
+
+Coordinate a = new Coordinate(3,3);
+Coordinate b = new Coordinate(2, 3);
+Coordinate c = new Coordinate(2, 2);
+
+Console.WriteLine($"Is coordinate a adjacent to coordinate b? {Coordinate.AreAdjacent(a,b)}" );
+Console.WriteLine($"Is coordinate b adjacent to coordinate c? {Coordinate.AreAdjacent(b,c)}" );
+Console.WriteLine($"Is coordinate a adjacent to coordinate c? {Coordinate.AreAdjacent(a,c)}" );
+
+public readonly struct Coordinate
+{
+    public int Row { get; }
+    public int Column { get; }
+
+    public Coordinate(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+    public static bool AreAdjacent(Coordinate a, Coordinate b)
+    {
+        int rowChange = a.Row - b.Row;
+        int columnChange = a.Column - b.Column;
+
+        if (Math.Abs(rowChange) <= 1 && columnChange == 0) return true;
+        if (Math.Abs(columnChange) <= 1 && columnChange == 0) return true;
+
+        return false;
+    }
+    
+}
