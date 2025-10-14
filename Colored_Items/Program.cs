81763bb686c0b83fff44cec84137837133a9fa61
rebeccalouw <louw.rebecca.m@gmail.com>
2025-10-14 14:28:27 +0200

    New challenges added

---
+++ Colored_Items/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Challenge: Colored Items
+
+var blueSword = new ColoredItem<Sword>(new Sword(), ConsoleColor.Blue);
+var redBow = new ColoredItem<Bow>(new Bow(), ConsoleColor.Red);
+var greenAxe = new ColoredItem<Axe>(new Axe(), ConsoleColor.Green);
+
+blueSword.Display();
+redBow.Display();
+greenAxe.Display();
+public class ColoredItem<T>
+{
+    public T Item { get; }
+    public ConsoleColor Colour { get; }
+
+    public ColoredItem(T item, ConsoleColor colour)
+    {
+        Item = item;
+        Colour = colour;
+    }
+
+    public void Display()
+    {
+        Console.ForegroundColor = Colour;
+        Console.WriteLine(Item);
+    }
+}
+
+public class Sword
+{
+    public override string ToString() => "Sword";
+}
+
+public class Bow
+{
+    public override string ToString() => "Bow";
+}
+
+public class Axe {
+    public override string ToString() => "Axe";
+}
---
+++ List_Of_Commands/Program.cs
@@ -0,0 +1,85 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// Challenge: List of Commands
+
+Robot robot = new Robot();
+
+while (true)
+{
+    string? choice = Console.ReadLine();
+
+    if (choice == "stop") break;
+
+    robot.Commands.Add(choice switch
+    {
+        "on" => new OnCommand(),
+        "off" => new OffCommand(),
+        "north" => new NorthCommand(),
+        "south" => new SouthCommand(),
+        "east" => new EastCommand(),
+        "west" => new WestCommand()
+    });
+}
+
+robot.Run();
+
+public class OffCommand : IRobotCommand
+{
+    public void Run(Robot robot) => robot.IsPowered = false;
+}
+
+public class OnCommand : IRobotCommand
+{
+    public void Run(Robot robot) => robot.IsPowered = true;
+}
+
+public class NorthCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.Y++;
+    }
+}
+
+public class SouthCommand : IRobotCommand {
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.Y--;
+    }
+}
+
+public class EastCommand : IRobotCommand {
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.X++;
+    }
+}
+
+public class WestCommand : IRobotCommand {
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.X--;
+    }
+}
+
+public interface IRobotCommand
+{
+    void Run(Robot robot);
+}
+
+public class Robot
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsPowered { get; set; }
+    public List<IRobotCommand?> Commands { get; } = new List<IRobotCommand?>();
+    public void Run()
+    {
+        foreach (IRobotCommand? command in Commands)
+        {
+            command?.Run(this);
+            Console.WriteLine($"[{X} {Y} {IsPowered}]");
+        }
+    }
+}
