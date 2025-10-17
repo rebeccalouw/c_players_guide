6fe1614415979aa5d9282ec441b7c381be6964da
rebeccalouw <louw.rebecca.m@gmail.com>
2025-10-17 16:43:36 +0200

    new challenges added

---
+++ Excepti_s_Game/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Challenge: Excepti's Game
+
+string currentPlayer = "Player 1";
+
+try
+{
+    int badCookie = new Random().Next(10);
+    List<int> previousGuesses = new List<int>();
+    
+    Console.WriteLine(
+        "There are nine delicious chocolate chip cookies around, but also one oatmeal raising cookie.");
+    Console.WriteLine("The player that picks the bad cookie is out!");
+
+    while (true)
+    {
+        int choice = -1;
+        bool previouslyGuessed = true;
+        
+        do
+        {
+            Console.WriteLine($"{currentPlayer}, choose a number between 1 and 9.");
+            
+            choice = Convert.ToInt32(Console.ReadLine());
+            previouslyGuessed = previousGuesses.Contains(choice);
+            
+            if (previouslyGuessed) Console.WriteLine("That number has been guessed before.");
+        } while (previouslyGuessed == true);
+        
+        if (choice == badCookie) throw new Exception();
+        
+        currentPlayer = currentPlayer == "Player 1" ? "Player 2" : "Player 1";
+        previousGuesses.Add(choice);
+    }
+    
+}
+catch (Exception)
+{   
+    string winner = currentPlayer == "Player 1" ? "Player 2" : "Player 1";
+    Console.WriteLine($"Oh no, you picked the bad cookie, you lose! {winner} is the winner!");
+}
+
+
+// If it wasn't necessary to use the exception because of the exercise instructions, I'd have handled the handled everything with if statements
---
+++ The_Sieve/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Challenge: The Sieve
+
+Sieve sieve = PickFilter();
+
+while (true)
+{
+    Console.Write("Enter a number: ");
+    int input = Convert.ToInt32(Console.ReadLine());
+    
+    string veredict = sieve.IsGood(input) ? "good" : "bad";
+    Console.WriteLine($"The number is {veredict}.");
+}
+
+bool IsEven(int number) => number % 2 == 0;
+bool IsPositive(int number) => number > 0;
+bool IsMultipleOfTen(int number) => number % 10 == 0;
+
+Sieve PickFilter()
+{
+    Console.WriteLine("Which filter would you like to use? 1 - Even, 2 - Positive, 3 - Multiple of ten: ");
+    string? choiceText = Console.ReadLine();
+    int.TryParse(choiceText, out int choice);
+
+    Func<int, bool> rule = choice switch
+    {
+        1 => IsEven,
+        2 => IsPositive,
+        3 => IsMultipleOfTen,
+        _ => IsEven
+    };
+    
+    Console.WriteLine($"Using filter {(choice == 2 ? "Positive" : choice == 3 ? "Multiple of ten" : "Even")}.");
+    return new Sieve(rule);
+}
+
+public class Sieve
+{
+    private readonly Func<int, bool> _decide;
+
+    public Sieve(Func<int, bool> decisionFunction)
+    {
+        _decide = decisionFunction;
+    }
+    
+    public bool IsGood(int number) => _decide(number);
+}
