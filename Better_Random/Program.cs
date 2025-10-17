fc37f134c286a4eb9b07538181fb9b99637f158b
rebeccalouw <louw.rebecca.m@gmail.com>
2025-10-17 09:51:53 +0200

    New challenges added

---
+++ Better_Random/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Challenge: Better Random
+
+Random random = new Random();
+
+Console.WriteLine(random.NextDouble(15));
+Console.WriteLine(random.NextDouble(7));
+Console.WriteLine(random.NextString("up", "down", "left", "right"));
+Console.WriteLine(random.CoinFlip());
+Console.WriteLine(random.CoinFlip(0.3));
+
+public static class RandomExtensions
+{
+    public static double NextDouble(this Random random, double max)
+    {
+        return random.NextDouble() * max;
+    }
+
+    public static string NextString(this Random random, params string[] choices)
+    {   
+        if (choices == null || choices.Length == 0)
+            Console.WriteLine("Please enter options");
+        
+        int index = random.Next(choices.Length);
+        return choices[index];
+    }
+
+    public static bool CoinFlip(this Random random, double chanceOfHeads = 0.5)
+    {
+        if (chanceOfHeads < 0) chanceOfHeads = 0;
+        if (chanceOfHeads > 1) chanceOfHeads = 1;
+        
+        return random.NextDouble() < chanceOfHeads;
+    }
+}
---
+++ Safer_Number_Crunching/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Challenge: Safer Number Crunching
+
+int number;
+double decimalNumber;
+bool trueFalse;
+
+
+do
+{
+    Console.WriteLine("Hey there, please enter a whole number, no decimals, please. ");
+} while(!int.TryParse(Console.ReadLine(), out number));
+Console.WriteLine(number);
+
+
+do
+{
+    Console.WriteLine("Please enter a number: ");
+} while (!double.TryParse(Console.ReadLine(), out decimalNumber));
+
+
+do
+{
+    Console.WriteLine("Please enter true or false: ");
+} while (!bool.TryParse(Console.ReadLine(), out trueFalse));
+
