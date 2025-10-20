e2662b18e2f1c1c6ff8ab7c2eb562a19c6586b70
rebeccalouw <louw.rebecca.m@gmail.com>
2025-10-20 16:05:35 +0200

    Add Charberry_Trees and The_Lambda_Sieve

---
+++ Charberry_Trees/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Challenge: Charberry Trees
+
+CharberryTree tree = new CharberryTree();
+Notifier notifier = new Notifier(tree);
+Harvester harvester = new Harvester(tree);
+
+
+while (true)
+    tree.MaybeGrow();
+public class CharberryTree
+{
+    private Random _random = new Random();
+    public bool Ripe { get; set; }
+
+    public event Action? Ripened;
+    public void MaybeGrow()
+    {
+// Only a tiny chance of ripening each time, but we try a lot!
+        if (_random.NextDouble() < 0.00000001 && !Ripe)
+        {
+            Ripe = true;
+            Ripened?.Invoke();
+        }
+    }
+
+    public void Harvest()
+    {
+        if (Ripe)
+            Ripe = false;
+    }
+}
+
+public class Notifier
+{
+    public Notifier(CharberryTree tree)
+    {
+        tree.Ripened += OnRipened;
+    }
+
+    private void OnRipened() =>
+        Console.WriteLine($"[{DateTime.Now:T}] A charberry tree has ripened!");
+}
+
+public class Harvester
+{
+    private int _harvestCount = 0;
+    private readonly CharberryTree _tree;
+
+    public Harvester(CharberryTree tree)
+    {
+        _tree = tree;
+        tree.Ripened += OnRipened;
+    }
+
+    private void OnRipened()
+    {
+        _harvestCount++;
+        _tree.Harvest();
+        Console.WriteLine($"Harvested! Total harvest: {_harvestCount}");
+    }
+}
---
+++ The_Lambda_Sieve/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Challenge: The Lambda Sieve
+
+Sieve sieve = PickFilter();
+
+while (true)
+{
+    Console.Write("Enter a number: ");
+    int input = Convert.ToInt32(Console.ReadLine());
+    
+    string veredict = sieve.IsGood(input) ? "good" : "bad";
+    Console.WriteLine($"The number is {veredict}.");
+}
+Sieve PickFilter()
+{
+    Console.WriteLine("Which filter would you like to use? 1 - Even, 2 - Positive, 3 - Multiple of ten: ");
+    string? choiceText = Console.ReadLine();
+    int.TryParse(choiceText, out int choice);
+
+    Func<int, bool> rule = choice switch
+    {
+        1 => n => n % 2 == 0,
+        2 => n => n > 0,
+        3 => n => n % 10 == 0,
+        _ => n => n % 2 == 0
+    };
+    
+    Console.WriteLine($"Using filter {(choice == 2 ? "Positive" : choice == 3 ? "Multiple of ten" : "Even")}.");
+    return new Sieve(rule);
+}
+
+public class Sieve
+{
+    private readonly Func<int, bool> _decide;
+
+    public Sieve(Func<int, bool> decisionFunction)
+    {
+        _decide = decisionFunction;
+    }
+    
+    public bool IsGood(int number) => _decide(number);
+}
