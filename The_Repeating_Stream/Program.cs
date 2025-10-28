785112b6f812a1950288e39f563bb34b09ee574f
rebeccalouw <louw.rebecca.m@gmail.com>
2025-10-28 14:39:07 +0200

    New challenges added

---
+++ The_Repeating_Stream/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// Challenge: The Repeating Stream
+
+RecentNumbers recentNumbers = new RecentNumbers {MostRecent = -1, SecondMostRecent = -2};
+Thread generatingThread = new Thread(GenerateNumbers);
+generatingThread.Start(recentNumbers);
+
+while (true)
+{
+    Console.ReadKey(false);
+
+    bool isRepeated;
+    lock(recentNumbers) 
+        isRepeated = recentNumbers.MostRecent == recentNumbers.SecondMostRecent;
+    
+    if (isRepeated) Console.WriteLine("You spotted a duplicate number!");
+    else Console.WriteLine("Ops, that's not a duplicate!");
+}
+void GenerateNumbers(object obj)
+{
+    if (obj == null || obj is not RecentNumbers) return;
+    
+    RecentNumbers recentNumbers = (RecentNumbers)obj;
+    Random random = new Random();
+
+    while (true)
+    {
+        int nextNumber = random.Next(10);
+
+        lock (recentNumbers)
+        {   
+            recentNumbers.SecondMostRecent = recentNumbers.MostRecent;
+            recentNumbers.MostRecent = nextNumber;
+        }
+        
+        Console.WriteLine($"Generated number is: {nextNumber}");
+        Thread.Sleep(1000);
+    }
+}
+
+public class RecentNumbers
+{
+    public int MostRecent { get; set; }
+    public int SecondMostRecent { get; set; }
+}
---
+++ The_Three_Lenses/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Challenge: The Three Lenses
+
+int[] data = { 1, 9, 2, 8, 3, 7, 4, 6, 5 };
+
+foreach (int number in Procedural(data))
+    Console.Write($"{number} ");
+Console.WriteLine();
+
+foreach (int number in QuerySyntax(data))
+    Console.Write($"{number} ");
+Console.WriteLine();
+
+foreach (int number in MethodSyntax(data))
+    Console.Write($"{number} ");
+Console.WriteLine();
+
+
+IEnumerable<int> Procedural(int[] nums)
+{
+    var evens = new List<int>();
+    foreach (var n in nums)
+        if (n % 2 == 0) evens.Add(n);
+    
+    evens.Sort();
+
+    for (int i = 0; i < evens.Count; i++)
+        evens[i] = evens[i] * 2;
+    
+    return evens;
+}
+
+IEnumerable<int> QuerySyntax(int[] nums)
+{
+    return from n in nums
+        where n % 2 == 0
+        orderby n
+        select n * 2;
+}
+
+IEnumerable<int> MethodSyntax(int[] nums)
+{
+    return nums
+        .Where(n => n % 2 == 0)
+        .OrderBy(n => n)
+        .Select(n => n * 2);
+}
